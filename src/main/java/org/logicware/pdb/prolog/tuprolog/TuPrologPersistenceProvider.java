81c3f633ab3817ffb2289c5eb50bb149ce20514a
Jose Zalacain <jzalacain@delnet.cu>
2018-12-12 15:11:27 +0100

    Update JPA related

---
+++ src/main/java/org/logicware/pdb/prolog/tuprolog/TuPrologPersistenceProvider.java
@@ -0,0 +1,9 @@
+package org.logicware.pdb.prolog.tuprolog;
+
+import javax.persistence.spi.PersistenceProvider;
+
+import org.logicware.database.jpa.JpaAbstractProvider;
+
+public class TuPrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
+
+}
