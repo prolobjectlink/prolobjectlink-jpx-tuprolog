c003cf10501342d02d88bd7d3e7956062689f717
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-07 16:13:51 -0400

    Instance Persistence Provider OK

--- src/main/java/org/logicware/pdb/prolog/tuprolog/TuPrologPersistenceProvider.java
+++ src/main/java/org/logicware/pdb/prolog/tuprolog/TuPrologPersistenceProvider.java
@@ -24,6 +24,8 @@ import static org.logicware.db.XmlParser.XML;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 import javax.persistence.spi.PersistenceProvider;
 import javax.persistence.spi.PersistenceUnitInfo;
@@ -31,7 +33,6 @@ import javax.persistence.spi.PersistenceUnitInfo;
 import org.logicware.db.DatabaseEngine;
 import org.logicware.db.DatabaseSchema;
 import org.logicware.db.DatabaseUser;
-import org.logicware.db.HierarchicalCache;
 import org.logicware.db.Protocol;
 import org.logicware.db.Schema;
 import org.logicware.db.etc.Settings;
@@ -40,6 +41,8 @@ import org.logicware.db.jpa.JpaEntityManagerFactory;
 import org.logicware.db.jpa.JpaProperties;
 import org.logicware.db.jpa.spi.JPAPersistenceXmlParser;
 import org.logicware.db.memory.MemoryHierarchical;
+import org.logicware.db.persistent.EmbeddedHierarchical;
+import org.logicware.db.persistent.RemoteHierarchical;
 import org.logicware.db.util.JavaReflect;
 import org.logicware.logging.LoggerConstants;
 import org.logicware.logging.LoggerUtils;
@@ -47,11 +50,48 @@ import org.logicware.logging.LoggerUtils;
 public class TuPrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
 
 	public TuPrologPersistenceProvider() {
-//		JPAPersistenceXmlParser p = new JPAPersistenceXmlParser();
-//		URL persistenceXml = Thread.currentThread().getContextClassLoader().getResource(XML);
-//		Map<String, PersistenceUnitInfo> m = p.parsePersistenceXml(persistenceXml);
-//		// TODO factory by create database by protocol
-//		DatabaseEngine database = MemoryHierarchical.newInstance(m);
-//		entityManagerFactory = new JpaEntityManagerFactory(database);
+		JPAPersistenceXmlParser p = new JPAPersistenceXmlParser();
+		URL persistenceXml = Thread.currentThread().getContextClassLoader().getResource(XML);
+		Map<String, PersistenceUnitInfo> m = p.parsePersistenceXml(persistenceXml);
+		for (Entry<String, PersistenceUnitInfo> info : m.entrySet()) {
+			PersistenceUnitInfo unit = info.getValue();
+			String name = unit.getPersistenceUnitName();
+			Properties properties = unit.getProperties();
+			Settings settings = new Settings(properties.getProperty(JpaProperties.DRIVER));
+			URL url = null;
+			try {
+				System.setProperty("java.protocol.handler.pkgs", Protocol.class.getPackage().getName());
+				url = new URL(properties.getProperty(JpaProperties.URL).replace(URL_PREFIX, ""));
+				if (!url.getPath().substring(url.getPath().lastIndexOf('/') + 1).equals(name)) {
+					throw new MalformedURLException("The URL path don't have database named " + name);
+				}
+			} catch (MalformedURLException e) {
+				LoggerUtils.error(MemoryHierarchical.class, LoggerConstants.URL, e);
+			}
+
+			assert url != null;
+
+			// catch the rest of properties
+			String password = properties.getProperty(JpaProperties.PASSWORD);
+			String user = properties.getProperty(JpaProperties.USER);
+			DatabaseUser owner = new DatabaseUser(user, password);
+			Schema schema = new DatabaseSchema(url.getPath(), settings.getProvider(), settings, owner);
+			for (String managedClass : unit.getManagedClassNames()) {
+				schema.addClass(JavaReflect.classForName(managedClass), "");
+			}
+
+			DatabaseEngine e = null;
+			String protocol = url.getProtocol();
+			if (protocol.equalsIgnoreCase(Protocol.MEMPDB.toString())) {
+				e = MemoryHierarchical.newInstance(unit, properties);
+			} else if (protocol.equalsIgnoreCase(Protocol.REMPDB.toString())) {
+				e = RemoteHierarchical.newInstance(unit, properties);
+			} else if (protocol.equalsIgnoreCase(Protocol.FILE.toString())) {
+				e = EmbeddedHierarchical.newInstance(unit, properties);
+			}
+
+			entityManagerFactories.put(name, new JpaEntityManagerFactory(e, properties));
+
+		}
 	}
 }
