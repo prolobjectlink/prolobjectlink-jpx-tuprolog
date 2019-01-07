e9ae2000407adec84305af89bdaf995af197bdea
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-01-07 08:59:04 -0400

    Working in production persistence provider

--- src/main/java/org/logicware/pdb/prolog/tuprolog/TuPrologPersistenceProvider.java
+++ src/main/java/org/logicware/pdb/prolog/tuprolog/TuPrologPersistenceProvider.java
@@ -19,10 +19,39 @@
  */
 package org.logicware.pdb.prolog.tuprolog;
 
-import javax.persistence.spi.PersistenceProvider;
+import static org.logicware.db.XmlParser.XML;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import javax.persistence.spi.PersistenceProvider;
+import javax.persistence.spi.PersistenceUnitInfo;
+
+import org.logicware.db.DatabaseEngine;
+import org.logicware.db.DatabaseSchema;
+import org.logicware.db.DatabaseUser;
+import org.logicware.db.HierarchicalCache;
+import org.logicware.db.Protocol;
+import org.logicware.db.Schema;
+import org.logicware.db.etc.Settings;
 import org.logicware.db.jpa.JpaAbstractProvider;
+import org.logicware.db.jpa.JpaEntityManagerFactory;
+import org.logicware.db.jpa.JpaProperties;
+import org.logicware.db.jpa.spi.JPAPersistenceXmlParser;
+import org.logicware.db.memory.MemoryHierarchical;
+import org.logicware.db.util.JavaReflect;
+import org.logicware.logging.LoggerConstants;
+import org.logicware.logging.LoggerUtils;
 
 public class TuPrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
 
+	public TuPrologPersistenceProvider() {
+//		JPAPersistenceXmlParser p = new JPAPersistenceXmlParser();
+//		URL persistenceXml = Thread.currentThread().getContextClassLoader().getResource(XML);
+//		Map<String, PersistenceUnitInfo> m = p.parsePersistenceXml(persistenceXml);
+//		// TODO factory by create database by protocol
+//		DatabaseEngine database = MemoryHierarchical.newInstance(m);
+//		entityManagerFactory = new JpaEntityManagerFactory(database);
+	}
 }
