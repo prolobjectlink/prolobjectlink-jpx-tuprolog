ee6b77fe488a0ebcd6bc053ffd254a1d457a349f
Jose Zalacain <Jose Zalacain@AVALON>
2019-03-24 11:13:21 -0400

    Introduce database console

---
+++ src/main/java/org/prolobjectlink/pdb/prolog/tuprolog/TuPrologDatabaseConsole.java
@@ -0,0 +1,43 @@
+/*
+ * #%L
+ * prolobjectlink-jpx-tuprolog
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package org.prolobjectlink.pdb.prolog.tuprolog;
+
+import org.prolobjectlink.db.DatabaseConsole;
+import org.prolobjectlink.db.prolog.AbstractDatabaseConsole;
+import org.prolobjectlink.prolog.tuprolog.TuProlog;
+
+/**
+ * 
+ * @author Jose Zalacain
+ * @since 1.0
+ */
+public class TuPrologDatabaseConsole extends AbstractDatabaseConsole implements DatabaseConsole {
+
+	public TuPrologDatabaseConsole() {
+		super(new TuProlog());
+	}
+
+	public static void main(String[] args) {
+		new TuPrologDatabaseConsole().run(args);
+	}
+
+}
